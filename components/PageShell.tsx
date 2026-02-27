e8a557177b829e62ca86cfe50a760779c726a8d1
Mohamed OUIJJANE <ouijjanemohamed2024@gmail.com>
2026-02-27 15:55:22 +0000

    refactor(pages): standardize layout with PageShell

--- components/PageShell.tsx
+++ components/PageShell.tsx
@@ -13,11 +13,11 @@ export function PageShell({ children }: PageShellProps) {
     <div className="min-h-screen bg-white relative overflow-x-hidden">
       {/* Background layer identical to Home */}
       <AnimatedLines />
-      
+
       {/* Global Shell components */}
       <TopNav />
       <SocialRail />
-      
+
       {/* Page Content */}
       <main className="mx-auto max-w-5xl px-6 py-24 lg:px-8 relative z-10">
         {children}
