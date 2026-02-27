663e176e5ff9ecd3fcd7e24b6e122ef60fa1e776
Mohamed OUIJJANE <ouijjanemohamed2024@gmail.com>
2026-02-27 15:55:11 +0000

    refactor(bg): remove animated wavy lines and clean unused keyframes

--- components/AnimatedLines.tsx
+++ components/AnimatedLines.tsx
@@ -18,55 +18,6 @@ export const AnimatedLines = () => {
           backgroundPosition: "0 0, 0 0",
         }}
       />
-
-      {/* Animated Trend Lines */}
-      <svg
-        className="absolute inset-0 w-full h-full"
-        viewBox="0 0 1000 1000"
-        preserveAspectRatio="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <defs>
-          <linearGradient id="lineGradient" x1="0%" y1="0%" x2="100%" y2="0%">
-            <stop offset="0%" stopColor="#374151" stopOpacity="0" />
-            <stop offset="50%" stopColor="#374151" stopOpacity="0.22" />
-            <stop offset="100%" stopColor="#374151" stopOpacity="0" />
-          </linearGradient>
-        </defs>
-
-        {/* Animated paths with different timing and positions */}
-        <path
-          d="M-100 200 Q 250 150 500 250 T 1100 200"
-          fill="none"
-          stroke="url(#lineGradient)"
-          strokeWidth="2"
-          className="animate-float-slow"
-        />
-        <path
-          d="M-100 450 Q 300 550 600 400 T 1100 500"
-          fill="none"
-          stroke="url(#lineGradient)"
-          strokeWidth="1.5"
-          className="animate-float-medium"
-        />
-        <path
-          d="M-100 750 Q 400 650 700 800 T 1100 700"
-          fill="none"
-          stroke="url(#lineGradient)"
-          strokeWidth="2"
-          className="animate-float-fast"
-        />
-
-        {/* Extra lines for density */}
-        <path
-          d="M-100 100 Q 200 300 500 100 T 1100 200"
-          fill="none"
-          stroke="url(#lineGradient)"
-          strokeWidth="1"
-          className="animate-float-medium"
-          style={{ opacity: 0.5 }}
-        />
-      </svg>
     </div>
   );
 };
